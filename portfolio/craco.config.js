2a5a1d70fae09dbe0a58d76b7337cc86d9edeaeb
zippyg <51121726+zippyg@users.noreply.github.com>
2024-06-23 08:21:43 +0000

    5

---
+++ portfolio/craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+module.exports = {
+    webpack: {
+      configure: (webpackConfig) => {
+        webpackConfig.module.rules.push({
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto",
+        });
+        return webpackConfig;
+      },
+    },
+  };
+  
